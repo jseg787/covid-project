8a82e865111c99b777550993127b1299ef94cd15
jseg787 <jsegarra3@gmail.com>
2021-03-17 15:13:58 -0400

    add send email if event status is valid

--- app.js
+++ app.js
@@ -3,42 +3,87 @@ const cheerio = require('cheerio');
 const nodemailer = require('nodemailer');
 require('dotenv').config();
 
-// const url = 'https://www.sharp.com/health-classes/volunteer-registration-grossmont-center-covid-19-vaccine-clinic-2558';
+const url = 'https://www.sharp.com/health-classes/volunteer-registration-grossmont-center-covid-19-vaccine-clinic-2558';
 
-// const getData = async () => {
-// 	const { data } = await axios.get(url);
+async function main() {
+	const results = await getData();
+	const openings = checkData(results);
 
-// 	const $ = cheerio.load(data);
-// 	const sections = $('.section-date');
-
-// 	sections.each((i, el) => {
-// 		const date = $(el).find('.eight.columns > .truss > h2').text();
-// 		const time = $(el).find('.eight.columns > .truss > div:not(.large-cta)').text();
-// 		const address = $(el).find('address').text();
-// 		const status = $(el).find('.one-third.column > .truss').text();
-
-// 		return { date, time, address, status };
-// 	});
-// };
-
-// setInterval(getData, 5 * 60000);
-
-const transporter = nodemailer.createTransport({
-	service: 'gmail',
-	auth: {
-		user: process.env.EMAIL_NAME,
-		pass: process.env.EMAIL_PASSWORD
+	if (openings.length > 0) {
+		console.log('There are openings');
+		sendEmailNotification(openings);
+	} else {
+		console.log('There are no openings');
 	}
-});
+}
 
-const mailOptions = {
-	from: process.env.EMAIL_NAME,
-	to: 'arandomname@exmaple.com',
-	subject: 'Testing',
-	text: 'This is a test of nodemailer'
-};
+async function getData() {
+	const { data } = await axios.get(url);
 
-transporter
-	.sendMail(mailOptions)
-	.then((res) => console.log('message sent'))
-	.catch((err) => console.log('there was an error\n', err));
+	const $ = cheerio.load(data);
+	const sections = $('.section-date');
+	const results = [];
+
+	sections.each((i, el) => {
+		const date = $(el).find('.eight.columns > .truss > h2').text();
+		const time = $(el).find('.eight.columns > .truss > div:not(.large-cta)').text();
+		const address = $(el).find('address').text();
+		const status = $(el).find('.one-third.column > .truss').text();
+
+		results.push({ date, time, address, status });
+	});
+
+	return results;
+}
+
+function checkData(data) {
+	const openings = [];
+
+	data.forEach((el) => {
+		const status = el.status.trim();
+
+		if (status === 'More Info' || status === 'Classes and Events') {
+			openings.push(el);
+		}
+	});
+
+	return openings;
+}
+
+async function sendEmailNotification(openings) {
+	let msgText =
+		'There is an open class or event for Volunteer Registration: Grossmont Center COVID-19 Vaccine Clinic\n';
+
+	// opening has { date, time, address, status }
+	openings.forEach((el) => {
+		const openingMsg = `Event on ${el.date} from ${el.time} \nat location ${el.address.trim()}\nhas status ${el.status.trim()}\n\n`;
+		msgText += openingMsg;
+	});
+
+	const transporter = nodemailer.createTransport({
+		service: 'gmail',
+		auth: {
+			user: process.env.EMAIL_NAME,
+			pass: process.env.EMAIL_PASSWORD
+		}
+	});
+
+	const mailOptions = {
+		from: process.env.EMAIL_NAME,
+		to: 'example@examplemail.com',
+		subject: 'There is an open class or event for Volunteer Registration: Grossmont Center COVID-19 Vaccine Clinic',
+		text: msgText
+	};
+
+	try {
+		await transporter.sendMail(mailOptions);
+		console.log('Message sent');
+	} catch (err) {
+		console.log('An Error Occurred');
+		console.log(err);
+	}
+}
+
+main();
+
+setInterval(main(), 5 * 60000);
